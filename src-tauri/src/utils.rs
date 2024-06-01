4936acfd2ce53450df601f8c1d0be3111efa9e24
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-01 10:37:40 +0800

    feat: implement random message
    This fully implements the random message feature by utilizing the rand
    crate's `choose` function. We load the questions from `questions.json`
    to the constant QUESTIONS and parse it using `load_questions` and
    finally return the result to the `choose` call.

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,5 +1,18 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+const QUESTIONS: &str = include_str!("./questions.json");
+
+fn load_questions() -> Vec<String> {
+    serde_json::from_str(QUESTIONS).unwrap()
+}
+
 pub fn get_random_message() -> String {
-    // TODO: generate random messages
-    // This isn't random I know, I will implement it soon xD
-    "random message xD".to_owned()
+    let questions: Vec<String> = load_questions();
+    let mut rng = thread_rng();
+    // NOTE: we can afford clones here it's just a single string xD
+    questions
+        .choose(&mut rng)
+        .unwrap_or(&"Why are you gae?".to_owned())
+        .to_owned()
 }
